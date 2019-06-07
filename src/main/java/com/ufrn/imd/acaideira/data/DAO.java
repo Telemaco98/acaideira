1cb9407aa6a10156ab74408fbf6c7c61eb0b58a9
shirleytelemaco98@gmail.com <shirleytelemaco98@gmail.com>
2019-06-07 18:11:59 -0300

    Creating a DAO interface and refactoring the Restaurant class

---
+++ src/main/java/com/ufrn/imd/acaideira/data/DAO.java
@@ -0,0 +1,13 @@
+package com.ufrn.imd.acaideira.data;
+
+import com.ufrn.imd.acaideira.data.exception.DatabaseException;
+
+public interface DAO <Type>{
+	public void insert (Type type) throws DatabaseException;
+	
+	public Type select (int id) throws DatabaseException;
+	
+	public void update (Type type) throws DatabaseException;
+	
+	public void delete (Type type) throws DatabaseException;
+}
--- src/main/java/com/ufrn/imd/acaideira/data/RestauranteDao.java
+++ src/main/java/com/ufrn/imd/acaideira/data/RestauranteDao.java
@@ -1,7 +1,6 @@
 package com.ufrn.imd.acaideira.data;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,170 +10,163 @@ import java.util.List;
 import com.ufrn.imd.acadeira.domain.*;
 import com.ufrn.imd.acaideira.data.exception.DatabaseException;
 
-public class RestauranteDao {
-
-
-	
-	private Connection con;  
+public class RestauranteDAO implements DAO<Restaurante> {
+	private Connection connection;
+	private static RestauranteDAO restauranteDAO;
 	private Statement comando;
-	
-	public RestauranteDao() {
+
+	private RestauranteDAO() throws DatabaseException {
+		this.connection = ConnectionFactory.getConnection();
+	}
+
+	public static synchronized RestauranteDAO getInstance() throws DatabaseException {
+		if (restauranteDAO == null)
+			restauranteDAO = new RestauranteDAO();
+		return restauranteDAO;
+	}
+
+	private void startConnection() throws ClassNotFoundException, SQLException, DatabaseException {
+		connection = ConnectionFactory.getConnection();
+		comando = connection.createStatement();
+		System.out.println("Conectado!");
+	}
+
+	private void closeConnection() {
+		try {
+			comando.close();
+			connection.close();
+			System.out.println("Conexão Fechada");
+		} catch (SQLException e) {
+		}
 	}
 
 	public void update(Restaurante r) throws DatabaseException {
-        try {
-        	conectar();
-        	StringBuffer buffer = new StringBuffer();
-            buffer.append("UPDATE restaurante SET ");
-            buffer.append(returnFieldValuesBD(r));
-            buffer.append(" WHERE id_restaunte=");
-            buffer.append(r.getId_restaurante());
-            String sql = buffer.toString();
- 
+		try {
+			this.startConnection();
+			StringBuffer buffer = new StringBuffer();
+			buffer.append("UPDATE restaurante SET ");
+			buffer.append(returnFieldValuesBD(r));
+			buffer.append(" WHERE id_restaunte=");
+			buffer.append(r.getId_restaurante());
+			String sql = buffer.toString();
+
 			comando.executeUpdate(sql);
-			fechar();
-		} catch (SQLException e) {
+		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		} finally {
+			closeConnection();
 		}
 	}
-
 
 	public void insert(Restaurante r) throws DatabaseException {
 		try {
-				conectar();
-			
-		        StringBuffer buffer = new StringBuffer();
-		        buffer.append("INSERT INTO restaurante (");
-		        buffer.append(this.retornarCamposBD());
-		        buffer.append(") VALUES (");
-		        buffer.append(retornarValoresBD(r));
-		        buffer.append(")");
-		        String sql = buffer.toString();
-		        comando.executeUpdate(sql);
-		        
-		        fechar();
-		} catch (ClassNotFoundException e) {
+			this.startConnection();
+
+			StringBuffer buffer = new StringBuffer();
+			buffer.append("INSERT INTO restaurante (");
+			buffer.append(this.retornarCamposBD());
+			buffer.append(") VALUES (");
+			buffer.append(retornarValoresBD(r));
+			buffer.append(")");
+			String sql = buffer.toString();
+			comando.executeUpdate(sql);
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		} finally {
+			closeConnection();
 		}
 	}
 
-
-	public Restaurante search(int id) throws DatabaseException {
-		try{
-			conectar();
-			String sql = "SELECT * FROM EMPLOYEE WHERE id_restaurante = " + this.retornarValorStringBD(String.valueOf(id)); 
+	public Restaurante select(int id) throws DatabaseException {
+		try {
+			this.startConnection();
+			
+			String sql = "SELECT * FROM EMPLOYEE WHERE id_restaurante = "
+					+ this.retornarValorStringBD(String.valueOf(id));
 			ResultSet rs = comando.executeQuery(sql);
-			Restaurante r  = new Restaurante();
-			if (rs.next()){
+			Restaurante r = new Restaurante();
+			if (rs.next()) {
 				r.setNome(rs.getString("nome"));
 				r.setTipo(rs.getString("tipo"));
 				r.setEndereco(rs.getString("endereco"));
 			}
-			
-			fechar();
+
 			return r;
-		} catch(SQLException e){
+		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+		} finally {
+			this.closeConnection();
 		}
 		return null;
-		
-		
 	}
 
-
-	public void remove(Restaurante r) throws DatabaseException {
-
-        try {
-        	conectar();
-    		String sql ="DELETE FROM restaurante WHERE id_restaurante=" + this.retornarValorStringBD(String.valueOf(r.getId_restaurante()));
+	public void delete(Restaurante r) throws DatabaseException {
+		try {
+			this.startConnection();
+			String sql = "DELETE FROM restaurante WHERE id_restaurante="
+					+ this.retornarValorStringBD(String.valueOf(r.getId_restaurante()));
 			comando.executeUpdate(sql);
-			fechar();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		} finally {
+			this.closeConnection();
 		}
-
 	}
 
 	public List<Restaurante> retrieveRestaurantes() throws DatabaseException {
 		try {
-			conectar();
+			this.startConnection();
 			String sql = "SELECT * FROM restaurante";
 			ResultSet rs = comando.executeQuery(sql);
 			List<Restaurante> rest = new ArrayList<Restaurante>();
-			while (rs.next()){
+			while (rs.next()) {
 				Restaurante r = new Restaurante();
 				r.setNome(rs.getString("nome"));
 				r.setTipo(rs.getString("tipo"));
 				r.setEndereco(rs.getString("endereco"));
 				rest.add(r);
 			}
-			fechar();
+
 			return rest;
-		} catch(SQLException e){
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		} finally {
+			this.closeConnection();
 		}
 		return null;
 	}
 
-	private void conectar() throws ClassNotFoundException, SQLException, DatabaseException  {
-        con = ConnectionFactory.getConnection();  
-		comando = con.createStatement();  
-        System.out.println("Conectado!");     
-	}	  
-	
-	private void fechar() {  
-		try {  
-			comando.close();  
-			con.close();  
-			System.out.println("Conexão Fechada");  
-		} catch (SQLException e) {  
-		}  
-	}
-	
 	protected String retornarCamposBD() {
-    	return "nome, tipo, endereco";
-    }
-    
-    protected String returnFieldValuesBD(Restaurante r) {
+		return "nome, tipo, endereco";
+	}
 
-        StringBuffer buffer = new StringBuffer();
-        buffer.append("nome=");
-        buffer.append(retornarValorStringBD(r.getNome()));
-        buffer.append(", tipo=");
-        buffer.append(retornarValorStringBD(r.getTipo()));
-        buffer.append(", endereco=");
-        buffer.append(retornarValorStringBD(r.getEndereco()));
-        return buffer.toString();
-    }
-    
-    protected String retornarValoresBD(Restaurante r) {
-    	return
-	        retornarValorStringBD(r.getNome())
-	        + ", "
-	        + retornarValorStringBD(r.getTipo())
-	        + ", "
-	        + retornarValorStringBD(r.getEndereco());
-    }
-    
-    private String retornarValorStringBD(String valor) {
-        if (valor != null && !"".equals(valor)) {
-            valor = "'" + valor + "'";
-        } else {
-            valor = "'"+"'";
-        }
-        return valor;
-    }
+	protected String returnFieldValuesBD(Restaurante r) {
+
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("nome=");
+		buffer.append(retornarValorStringBD(r.getNome()));
+		buffer.append(", tipo=");
+		buffer.append(retornarValorStringBD(r.getTipo()));
+		buffer.append(", endereco=");
+		buffer.append(retornarValorStringBD(r.getEndereco()));
+		return buffer.toString();
+	}
+
+	protected String retornarValoresBD(Restaurante r) {
+		return retornarValorStringBD(r.getNome()) + ", " + retornarValorStringBD(r.getTipo()) + ", "
+				+ retornarValorStringBD(r.getEndereco());
+	}
+
+	private String retornarValorStringBD(String valor) {
+		if (valor != null && !"".equals(valor)) {
+			valor = "'" + valor + "'";
+		} else {
+			valor = "'" + "'";
+		}
+		return valor;
+	}
 }
-
-
-
