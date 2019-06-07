08cfd8040091d23f80bb610a89689c8dc3632111
shirleytelemaco98@gmail.com <shirleytelemaco98@gmail.com>
2019-06-07 17:33:17 -0300

    Adding the ConnectionFactory in the Project and the exceptionClass

---
+++ src/main/java/employee/data/ConnectionFactory.java
@@ -0,0 +1,41 @@
+package employee.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import employee.data.exception.DatabaseException;
+
+/**
+ * Class to do the connection with the database
+ * @author Shirley Ohara
+ *
+ */
+public class ConnectionFactory {
+	private static final String timezone = "?useTimezone=true&serverTimezone=UTC";
+	private static final String bd = "company";
+	private static final String url = "jdbc:mysql://localhost:3306/" + bd + timezone;
+	private static final String user = "root";
+	private static final String psw = "telemaco98";
+	private static final String driver = "com.mysql.cj.jdbc.Driver";
+	
+	public static Connection getConnection() throws DatabaseException {
+		try {
+			Class.forName(driver);
+			System.out.println("Chegay aq");
+			return DriverManager.getConnection(url, user, psw);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			ConnectionFactory.getConnection();
+			System.out.println("Connection sucessfull");
+		} catch (DatabaseException e) {
+			System.out.println("Connection failed");
+		}
+	} 
+}
---
+++ src/main/java/employee/data/exception/DatabaseException.java
@@ -0,0 +1,13 @@
+package employee.data.exception;
+
+/**
+ * Class to the database exceptions
+ * @author Shirley Ohara
+ */
+public class DatabaseException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException (String msg) {
+		super (msg);
+	}
+}
